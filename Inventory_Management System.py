baeb2cd9e9f9f486e70976128c548646d159a29f
Punyabratarath98 <jiten1997.punya@gmail.com>
2020-06-08 16:55:48 +0530

    Inventory_Management_System.py now returns the values fetched from the html form

--- Inventory_Management System.py
+++ Inventory_Management System.py
@@ -1,10 +1,22 @@
 from flask import Flask,redirect,url_for, render_template, request
 
 app = Flask(__name__)
-
-@app.route("/",methods=["POST","GET"])
+#@app.route("/",methods=["POST","GET"])
+@app.route("/index",methods=["POST","GET"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        dt = request.form["date"]
+        print("Date:",(dt))
+        product = request.form["product"]
+        print("Product:",(product))
+        units = request.form["units"]
+        print("Number of units:",(units))
+        price = request.form["price"]
+        print("Price:",(price))
+        return redirect(url_for("home"))
+    else:
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
