4d44b0c127ef966b1f2e1b42dc6da10821d9ffeb
SKS-WAR <sahoosudeep2010@gmail.com>
2020-06-10 11:14:11 +0530

    sendDataToFirebase.py is modified to send data to firebase from Inventory_Management_System.py

--- sendDataToFirebase.py
+++ sendDataToFirebase.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import connector
+try:
+    import AdminSDK
+except:
+    pass
+from firebase_admin import db
 
-name = 'SKS'
-desc = 'SKS WAR'
-price = '50'
-quantity = '500'
-row = '21'
 
-users_ref = ref
-users_ref.update({
-    row : {
-        'name' : name,
-        'desc' : desc,
-        'price' : price,
-        'quantity' : quantity
-    }
-})
+def sendData(name,desc,price,quantity):
+    row = "1"
+    ref = db.reference('/IMS')
+    users_ref = ref
+    users_ref.update({
+        row : {
+            'name' : name,
+            'desc' : desc,
+            'price' : price,
+            'quantity' : quantity
+        }
+    })
