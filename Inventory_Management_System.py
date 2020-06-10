86dbe0e264612ff24e77d03771fbfd9a98557fed
SKS-WAR <sahoosudeep2010@gmail.com>
2020-06-10 16:30:38 +0530

    Variables names are renamed to adjust to the database.

--- Inventory_Management_System.py
+++ Inventory_Management_System.py
@@ -16,14 +16,13 @@ def home():
         price = request.form["price"]
         #sending data to firebase
         try:
-            sendDataToFirebase.sendData(name=product_name,price=price,quantity=units)
+            sendDataToFirebase.sendData(name=product_name,date=dt,price=price,quantity=units)
             desktopNotification.notify("Data Inserted successfully")
         except :
             desktopNotification.notify("Some Issues Arised")
         return redirect(url_for("home"))
     else:
         return render_template("index.html")
-
 
 if __name__ == "__main__":
     webbrowser.open('http://127.0.0.1:5000')
