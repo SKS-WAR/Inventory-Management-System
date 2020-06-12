e331c45aeccf73f864241bf8e358cbd5cf6aa183
SKS-WAR <sahoosudeep2010@gmail.com>
2020-06-12 14:27:43 +0530

    Download features added. Minor fixes.

--- Inventory_Management_System.py
+++ Inventory_Management_System.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for, render_template, request
+from flask import Flask,redirect,url_for, render_template, request,send_file
 try:
     import sendDataToFirebase
 except :
@@ -16,13 +16,13 @@ def home():
         price = request.form["price"]
         #sending data to firebase
         try:
-            sendDataToFirebase.sendData(name=product_name,date=dt,price=price,quantity=units)
+            sendDataToFirebase.sendProductionData(name=product_name,date=dt,price=price,quantity=units)
             desktopNotification.notify("Data Inserted successfully")
         except :
             desktopNotification.notify("Some Issues Arised")
         return redirect(url_for("home"))
     else:
-        return render_template("index.html", )
+        return render_template("index.html")
 
 @app.route("/despatch",methods=["POST","GET"])
 def despatch():
@@ -33,13 +33,23 @@ def despatch():
         amount = request.form["price"]
         #sending data to firebase
         try:
-            sendDataToFirebase.sendData(name=p_name,date=d,price=amount,quantity=unts)
+            sendDataToFirebase.sendDespatchData(name=p_name,date=d,price=amount,quantity=unts)
             desktopNotification.notify("Data Inserted successfully")
         except :
             desktopNotification.notify("Some Issues Arised")
         return redirect(url_for("despatch"))
     else:
         return render_template("despatch.html")
+
+#to download a file use <a href = "{{url_for('download_report')}}">Download</a>
+@app.route("/download")
+def download():
+    return render_template("downlaod.html")
+	
+@app.route("/xyz")
+def download_report():
+    path = "robots.txt"
+    return send_file(path,as_attachment=True)
 
 if __name__ == "__main__":
     webbrowser.open('http://127.0.0.1:5000')
