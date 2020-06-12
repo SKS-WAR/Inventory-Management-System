ba97eaf8a55ac9fce336d74e05f184ae54667e65
SKS-WAR <sahoosudeep2010@gmail.com>
2020-06-12 14:29:22 +0530

    Diffent functions are created to send data of production and despatch to different child of the firebase.

--- sendDataToFirebase.py
+++ sendDataToFirebase.py
@@ -8,9 +8,28 @@ from firebase_admin import db
 import JSON_parser
 
 
-def sendData(name,date="",price="",quantity=""):
-    ref = db.reference('/IMS')
-    row = JSON_parser.transaction_count(ref.get())
+def sendProductionData(name,date="",price="",quantity=""):
+    ref = db.reference('/IMS/production/')
+    try:
+        row = JSON_parser.transaction_count(ref.get())
+    except:
+        row = "0"
+    users_ref = ref
+    users_ref.update({
+        row : {
+            'name' : name,
+            'date' : date,
+            'price' : price,
+            'quantity' : quantity
+        }
+    })
+
+def sendDespatchData(name,date="",price="",quantity=""):
+    ref = db.reference('/IMS/despatch/')
+    try:
+        row = JSON_parser.transaction_count(ref.get())
+    except:
+        row = "0"
     users_ref = ref
     users_ref.update({
         row : {
