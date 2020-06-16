817b3812c0a62b1ffef4d224b3bcb32671a24d59
SKS-WAR <sahoosudeep2010@gmail.com>
2020-06-16 16:31:34 +0530

    Added several functionality logout added session full fledged major corner cases recovered

--- Inventory_Management_System.py
+++ Inventory_Management_System.py
@@ -1,54 +1,79 @@
-from flask import Flask,redirect,url_for, render_template, request,send_file
+from flask import Flask,redirect,url_for, render_template, request,send_file,session
 try:
     import sendDataToFirebase
 except :
     pass
 import webbrowser
 import desktopNotification
-app = Flask(__name__)
+import firebase
 
+app = Flask(__name__)
+app.secret_key = "BIGsecretOfMine&plzDontDecrptMe"
+
+@app.route("/",methods=["POST","GET"])
 @app.route("/login",methods=["POST","GET"])
 def login():
     if request.method == "POST":
         name = request.form["uname"]
         password = request.form["password"]
-        return redirect(url_for("home"))
+        user = firebase.login(name,password)
+        session['user'] = user
+        if user != None :
+            return redirect(url_for("home"))
+        else:
+            return render_template('login.html')
     else:
         return render_template("login.html")
 
-@app.route("/",methods=["POST","GET"])
+@app.route("/product",methods=["POST","GET"])
 def home():
-    if request.method == "POST":
-        dt = request.form["date"]
-        product_name = request.form["product"]
-        units = request.form["units"]
-        price = request.form["price"]
-        #sending data to firebase
-        try:
-            sendDataToFirebase.sendProductionData(name=product_name,date=dt,price=price,quantity=units)
-            desktopNotification.notify("Data Inserted successfully")
-        except :
-            desktopNotification.notify("Some Issues Arised")
-        return redirect(url_for("home"))
+    if 'user' in session:
+        user = session["user"]
+        if request.method == "POST":
+            dt = request.form["date"]
+            product_name = request.form["product"]
+            units = request.form["units"]
+            price = request.form["price"]
+            #sending data to firebase
+            try:
+                sendDataToFirebase.sendProductionData(name=product_name,date=dt,price=price,quantity=units)
+                desktopNotification.notify("Data Inserted successfully")
+            except :
+                desktopNotification.notify("Some Issues Arised")
+            return redirect(url_for("user"))
+        else:
+            return render_template("index.html")
     else:
-        return render_template("index.html")
+        return redirect(url_for('login'))
+
+@app.route("/user")
+def user():
+    if 'user' in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/despatch",methods=["POST","GET"])
 def despatch():
-    if request.method == "POST":
-        d = request.form["date"]
-        p_name = request.form["product"]
-        unts = request.form["units"]
-        amount = request.form["price"]
-        #sending data to firebase
-        try:
-            sendDataToFirebase.sendDespatchData(name=p_name,date=d,price=amount,quantity=unts)
-            desktopNotification.notify("Data Inserted successfully")
-        except :
-            desktopNotification.notify("Some Issues Arised")
-        return redirect(url_for("despatch"))
+    if 'user' in session:
+        user = session["user"]
+        if request.method == "POST":
+            d = request.form["date"]
+            p_name = request.form["product"]
+            unts = request.form["units"]
+            amount = request.form["price"]
+            #sending data to firebase
+            try:
+                sendDataToFirebase.sendDespatchData(name=p_name,date=d,price=amount,quantity=unts)
+                desktopNotification.notify("Data Inserted successfully")
+            except :
+                desktopNotification.notify("Some Issues Arised")
+            return redirect(url_for("despatch"))
+        else:
+            return render_template("despatch.html")
     else:
-        return render_template("despatch.html")
+        return redirect(url_for('login'))
 
 #to download a file use <a href = "{{url_for('download_report')}}">Download</a>
 @app.route("/download")
@@ -60,6 +85,12 @@ def download_report():
     path = "robots.txt"
     return send_file(path,as_attachment=True)
 
+@app.route("/logout")
+def logout():
+    session.pop('user',None)
+    return redirect(url_for('login'))
+
+
 if __name__ == "__main__":
-    webbrowser.open('http://127.0.0.1:5000')
+    webbrowser.open('http://127.0.0.1:5000/')
     app.run()
