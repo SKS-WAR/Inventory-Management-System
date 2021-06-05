cfa4161c885edabd57217c709635a4fdc09aa344
SKS-WAR <39806889+SKS-WAR@users.noreply.github.com>
2021-06-05 17:31:26 +0530

    Update firebaseAPI_handler.py

--- firebaseAPI_handler.py
+++ firebaseAPI_handler.py
@@ -5,15 +5,7 @@ import pyrebase
 import JSON_parser
 
 
-config = {
-  'apiKey': "AIzaSyD4O8YcABrlEsYzYnEwO9CocUj9OnYak60",
-  'authDomain': "spesina-b1bcd.firebaseapp.com",
-  'databaseURL': "https://spesina-b1bcd.firebaseio.com",
-  'projectId': "spesina-b1bcd",
-  'storageBucket': "spesina-b1bcd.appspot.com",
-  'messagingSenderId': "324125011109",
-  'appId': "1:324125011109:web:441d07e0eb3ba293acb14c"
-}
+config = {}  //paste your config here
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
